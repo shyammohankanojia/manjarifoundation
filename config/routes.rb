eeb9583ab2968dee06c3280ab28da9a3ef6f7451
shyam <shyammohankanojia@gmail.com>
2016-01-25 16:23:37 +0530

    layout fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'home/about'
-  get 'home/contact'
+  get "contact", to: 'home#contact'
   get 'home/who_we_are'
   get 'home/our_mission'
 end
