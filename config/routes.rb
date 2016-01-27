0d0b46a419e4ad809235c07a22462767d7a93e82
AmarKanth <amarkanth.bollu@gmail.com>
2016-01-27 18:07:15 +0530

    changes routing format

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
   	root 'home#index'
   	
-  	get 'our_mission', to: 'home#our_mission'
-  	get 'how_we_started', to: 'home#how_we_started'
-  	get 'where_we_work', to: 'home#where_we_work'
+  	get 'our-mission', to: 'home#our_mission'
+  	get 'how-we-started', to: 'home#how_we_started'
+  	get 'where-we-work', to: 'home#where_we_work'
 
   	get 'principals', to: 'home#principals'
   	get 'organization', to: 'home#organization'
-  	get 'livlihood_programs', to: 'home#livlihood_programs'
-  	get 'education_programs', to: 'home#education_programs'
-  	get 'youth_development_programs', to: 'home#youth_development_programs'
+  	get 'livlihood-programs', to: 'home#livlihood_programs'
+  	get 'education-programs', to: 'home#education_programs'
+  	get 'youth-development-programs', to: 'home#youth_development_programs'
 
-  	get 'our_supporters', to: 'home#our_supporters'
+  	get 'our-supporters', to: 'home#our_supporters'
 
   	get 'resources', to: 'home#resources'
 
