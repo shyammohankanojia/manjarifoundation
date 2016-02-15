e79d0090ffeb6eef846d1ece16f7aa24e02c4221
Shyam <shyammohankanojia@gmail.com>
2016-02-16 01:03:06 +0530

    image size reduced

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: 'gmail.com',
+    domain: 'manjarifoundation.in',
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: 'amar.funride@gmail.com',
